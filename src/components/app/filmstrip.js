701d4e36b84a7899959494c84a32a55bf6aa9063
ByLiZhang <32698353+ByLiZhang@users.noreply.github.com>
2018-06-11 16:23:41 -0700

    Synced thumbnails with slides

--- src/components/app/filmstrip.js
+++ src/components/app/filmstrip.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 
 class FilmStrip extends Component {
 	render() {
+		const {currentIndex} = this.props;
 		const thumbnails = this.props.slides.map((item, index)=>{
+			const name = (index===currentIndex) ? 'thumbnail active' : 'thumbnail';
 			return (
-				<div className='thumbnail' key={index} style={{backgroundImage:`url(${item.src})`}}
+				<div className={name} key={index} style={{backgroundImage:`url(${item.src})`}}
 				onClick={()=>this.props.handleThumbnailClick(index)}></div>
 			);
 		});
