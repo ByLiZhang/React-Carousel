6b14fce6e587eb59d327ac6983dc059621356b88
ByLiZhang <32698353+ByLiZhang@users.noreply.github.com>
2018-06-11 14:07:21 -0700

    Completed initial setup for packages and directory structure

---
+++ src/components/app/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../../assets/styles/app.scss';
+
+class App extends Component {
+
+	render() {
+		return (
+			<div>Working</div>
+		);
+	}
+
+} 
+
+export default App;
---
+++ webpack.common.js
@@ -0,0 +1,78 @@
+const { resolve } = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const env = process.env.NODE_ENV;
+
+module.exports = {
+    output: {
+        filename: 'bundle.js',
+        path: resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    context: resolve(__dirname, 'src'),
+
+    module: {
+        rules: [
+            {
+                test: /\.js|.jsx?$/,
+                use: ['babel-loader'],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: env === 'production'
+                    ? ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: ['css-loader?minimize']
+                    })
+                    : ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    env !== 'production'
+                        ? 'style-loader'
+                        : MiniCssExtractPlugin.loader,
+                    'css-loader?minimize',
+                    'resolve-url-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: ['file-loader?hash=sha512&digest=hex&name=assets/images/[name].[ext]',
+                    'image-webpack-loader?bypassOnDebug']
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        outputPath: 'assets/fonts/'
+                    }
+                }
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: false,
+            template: require('html-webpack-template'),
+            appMountId: 'root',
+            meta: [
+                // EXAMPLE:
+                // {
+                //     name: '',
+                //     content: ''
+                // }
+            ],
+            links: [],
+            mobile: true,
+            lang: 'en-us',
+            title: 'Carousel Demo'
+        })
+  ]
+};
+
+   
